adde03ba770a49eccb51f77a561449817adec42e
root <root@192.168.26.131>
2020-01-23 15:52:32 +0800

    commplete the configure file

--- shadowsocks/install.sh
+++ shadowsocks/install.sh
@@ -1,41 +1,26 @@
-if test type pip
+yum install epel-release -y
+yum install gcc gettext autoconf libtool automake make pcre-devel asciidoc xmlto c-ares-devel libev-devel libsodium-devel mbedtls-devel git -y
+mv config ~/config_ss_temp
+cd ~
+git clone https://github.com/shadowsocks/shadowsocks-libev.git
+cd shadowsocks-libev/
+git submodule update --init
+./autogen.sh && ./configure && make
+make install
+cd /etc
+mkdir shadowsocks
+cd shadowsocks
+mv ~/config_ss_temp ./config
+firewall-cmd --zone=public --add-port=80/tcp --permanent
+firewall-cmd --reload
+read -p "Do you want to load ss now? (y/n): " flag
+if [ "$flag" = "y" -o "$flag" = "Y" ]
 then
-    echo Have pip
-else
-    yum install epel-release
-    yum install pip
-fi
-pip install shadowsocks
-if [ -f /etc/shadowsocks.json || touch /etc/shadowsocks.json ]
+ss-server -c /etc/shadowsocks/config
+echo "Start ss!"
+elif [ "$flag" = "n" -o "$flag" = "N" ]
 then
-    echo '{ "server":"my_server_ip",
-    "server_port":8388,
-    "local_address": "127.0.0.1",
-    "local_port":1080,
-    "password":"mypassword",
-    "timeout":300,
-    "method":"aes-256-cfb",
-    "fast_open": false
-}' > /etc/shadowsocks.json
-fi
-
-cd 
-if [ -d ~/bin || mkdir bin ]
-then
-    cd bin
-    touch shadowsocks
-    echo '#!/bin/bash
-if (($#==0))
-then
-        echo Give me a command!
-elif [[ "$1" = "start" ]]
-then
-        ssserver -c /etc/shadowsocks.json -d start
-        #ssserver -p 443 -k password -m rc4-md5 --user nobody -d start
-elif [[ "$1" = "stop" ]]
-then
-        ssserver -c /etc/shadowsocks.json -d stop
-        #ssserver -d stop
-fi
-' > shadowsocks
+echo "success !"
+else 
+echo -e "\nWrong input!\nDo not start ss!"
 fi
